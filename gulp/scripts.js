2d9aa403d45db27fa18957ceeb9eea518d75f964
lebedev-023046 <lebedev.023046@gmail.com>
2024-09-10 16:25:08 +0300

    feat: add main page instructions basic section

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,25 +1,27 @@
 import browserSyncModule from "browser-sync";
 import { dest, src } from "gulp";
 import concat from "gulp-concat";
-import plumber from "gulp-plumber";
+// import plumber from "gulp-plumber";
 import terser from "gulp-terser";
 const browserSync = browserSyncModule.create();
 
 export function scripts() {
-  return src("./src/js/index.js")
-    .pipe(
-      plumber({
-        errorHandler: function (err) {
-          notify.onError({
-            title: "JS Error",
-            message: "Error: <%= error.message %>",
-          })(err);
-          this.emit("end");
-        },
-      })
-    )
-    .pipe(concat("index.min.js"))
-    .pipe(terser())
-    .pipe(dest("./src/js"))
-    .pipe(browserSync.stream());
+  return (
+    src("./src/js/index.js")
+      // .pipe(
+      //   plumber({
+      //     errorHandler: function (err) {
+      //       notify.onError({
+      //         title: "JS Error",
+      //         message: "Error: <%= error.message %>",
+      //       })(err);
+      //       this.emit("end");
+      //     },
+      //   })
+      // )
+      .pipe(concat("index.min.js"))
+      .pipe(terser())
+      .pipe(dest("./src/js"))
+      .pipe(browserSync.stream())
+  );
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,43 @@ const arr = [1, 2, 3, 4, 5, 6, 7];
 const main = document.querySelector(".main");
 
 console.log(arr);
+
+// HOME INSTRUCTION SECTION
+
+const radioButtonsBlock = document.querySelector(".view__rbs");
+const radioButtons = document.querySelectorAll(".view__rb");
+
+// get instructions content
+
+const headingEl = document.querySelector(".view-heading");
+const paragraphEl = document.querySelector(".view-paragraph");
+const instructionLinkEl = document.querySelector(".view-inctruction-link");
+const videoSrcEl = document.querySelector(".view-videoSrc");
+const videoEl = document.querySelector(".view__video");
+
+fetch("../json/instructions.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to load JSON");
+    }
+    return response.json();
+  })
+  .then((instructionsSourses) => {
+    radioButtonsBlock.addEventListener("click", (event) => {
+      if (event.target && event.target.type === "radio") {
+        const instructionId = event.target.id;
+        try {
+          const { heading, paragraph, instructionLink, videoSrc } =
+            instructionsSourses[instructionId];
+
+          headingEl.textContent = heading;
+          paragraphEl.textContent = paragraph;
+          instructionLinkEl.href = instructionLink;
+          videoSrcEl.src = videoSrc;
+          videoEl.load();
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  });
