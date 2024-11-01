6d9f3f874b5210c03d4112d97fd342172c3d7228
l1ghbe <ee1zet25@gmail.com>
2024-11-01 02:26:31 +0100

    chore: moved the token to a separate file and changed the text for a message

--- src/js/telegram.js
+++ src/js/telegram.js
@@ -1,43 +1,51 @@
-document
-  .getElementById("telegramForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+fetch("./config/config.json")
+  .then((response) => response.json())
+  .then((config) => {
+    const TOKEN = config.privateBotToken;
+    const CHAT_ID = config.privateChatId;
 
-    const botToken = "";
-    const chatId = "";
+    document
+      .getElementById("telegramForm")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const name = document.getElementById("form-name").value;
-    const telegram = document.getElementById("form-tg").value;
-    const email = document.getElementById("form-email").value;
+        const botToken = TOKEN;
+        const chatId = CHAT_ID;
 
-    const message = `
-        New Form Submission:
-        - Name: ${name}s
-        - Telegram: ${telegram}
-        - Email: ${email}
+        const name = document.getElementById("form-name").value;
+        const telegram = document.getElementById("form-tg").value;
+        const email = document.getElementById("form-email").value;
+
+        const message = `
+        Новая заявка 📧:
+Имя: ${name}
+Telegram: @${telegram}
+Почта: ${email}
     `;
 
-    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: message,
-        parse_mode: "HTML",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.ok) {
-          alert("Message sent successfully!");
-        } else {
-          alert("Error sending message.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("There was an error sending the message.");
+        fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: message,
+            parse_mode: "HTML",
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.ok) {
+              alert("Message sent successfully!");
+            } else {
+              alert("Error sending message.");
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            alert("There was an error sending the message.");
+          });
       });
-  });
+  })
+  .catch((err) => console.log("Error occured -", err));
